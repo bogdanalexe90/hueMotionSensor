0f1a56f5e1d31af64e6b6175acd709f46b110d7a
Bogdan Alexe <bogdanalexe90@gmail.com>
2018-09-07 17:16:45 +0300

    Removed unnecessary import

--- devicetypes/bogdanalexe90/hue-motion-sensor.src/hue-motion-sensor.groovy
+++ devicetypes/bogdanalexe90/hue-motion-sensor.src/hue-motion-sensor.groovy
@@ -13,7 +13,6 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-import physicalgraph.zigbee.clusters.iaszone.ZoneStatus
 import physicalgraph.zigbee.zcl.DataType
  
  
